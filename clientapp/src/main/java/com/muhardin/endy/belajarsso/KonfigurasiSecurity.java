89607b8b819cef47131aa96127452d819827f379
Endy Muhardin <endy.muhardin@gmail.com>
2017-02-09 08:59:20 +0700

    implement login dengan google

--- clientapp/src/main/java/com/muhardin/endy/belajarsso/KonfigurasiSecurity.java
+++ clientapp/src/main/java/com/muhardin/endy/belajarsso/KonfigurasiSecurity.java
@@ -1,18 +1,19 @@
 package com.muhardin.endy.belajarsso;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity(debug = true)
+@EnableOAuth2Sso
 public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/protected.html").authenticated()
-                .anyRequest().permitAll()
-                .and().httpBasic();
+                .anyRequest().permitAll();
     }
     
 }
